7393eebd48faab4d2658c62e79b81d9e8ea028a9
dodoqqww <dodoqqww@gmail.com>
2021-08-22 15:56:20 +0200

    asd2213

--- lib/src/MonthSelector.dart
+++ lib/src/MonthSelector.dart
@@ -65,25 +65,21 @@ class MonthSelectorState extends State<MonthSelector> {
 
   Widget _getMonthButton(final DateTime date, final String locale) {
     final bool isEnabled = _isEnabled(date);
-    return OutlinedButton(
-      style: ButtonStyle(
-        shape: MaterialStateProperty.all(
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0))),
-      ),
+    return FlatButton(
       onPressed: isEnabled
           ? () => widget.onMonthSelected(DateTime(date.year, date.month))
           : null,
-      // color: date.month == widget.selectedDate!.month &&
-      //         date.year == widget.selectedDate!.year
-      //     ? Theme.of(context).accentColor
-      //     : null,
-      // textColor: date.month == widget.selectedDate!.month &&
-      //         date.year == widget.selectedDate!.year
-      //     ? Theme.of(context).accentTextTheme.button!.color
-      //     : date.month == DateTime.now().month &&
-      //             date.year == DateTime.now().year
-      //         ? Theme.of(context).accentColor
-      //         : null,
+      color: date.month == widget.selectedDate!.month &&
+              date.year == widget.selectedDate!.year
+          ? Theme.of(context).accentColor
+          : null,
+      textColor: date.month == widget.selectedDate!.month &&
+              date.year == widget.selectedDate!.year
+          ? Theme.of(context).accentTextTheme.button!.color
+          : date.month == DateTime.now().month &&
+                  date.year == DateTime.now().year
+              ? Theme.of(context).accentColor
+              : null,
       child: Text(
         DateFormat.MMM(locale).format(date),
       ),
